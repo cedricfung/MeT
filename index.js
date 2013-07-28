a5f186acae6ae9ec9f6387b350f3da39cf2a73c3
Cedric Fung <cedric@vec.io>
2013-07-29 00:46:07 +0800

    Basic stylesheets and highlight with CodeMirror runmode

--- index.js
+++ index.js
@@ -1,9 +1,52 @@
 jQuery(function ($) { $(document).ready(function(){
 
+  var gDiv = $('<div/>');
+
+  var getCMMode = (function () {
+    var aliases = {
+      html: "htmlmixed",
+      js: "javascript",
+      json: "application/json",
+      c: "text/x-csrc",
+      "c++": "text/x-c++src",
+      java: "text/x-java",
+      csharp: "text/x-csharp",
+      "c#": "text/x-csharp",
+      scala: "text/x-scala"
+    };
+
+    var i, modes = {}, mimes = {}, mime;
+
+    var list = [];
+    for (var m in CodeMirror.modes)
+      if (CodeMirror.modes.propertyIsEnumerable(m)) list.push(m);
+    for (i = 0; i < list.length; i++) {
+      modes[list[i]] = list[i];
+    }
+    var mimesList = [];
+    for (var m in CodeMirror.mimeModes)
+      if (CodeMirror.mimeModes.propertyIsEnumerable(m))
+        mimesList.push({mime: m, mode: CodeMirror.mimeModes[m]});
+    for (i = 0; i < mimesList.length; i++) {
+      mime = mimesList[i].mime;
+      mimes[mime] = mimesList[i].mime;
+    }
+
+    for (var a in aliases) {
+      if (aliases[a] in modes || aliases[a] in mimes)
+        modes[a] = aliases[a];
+    }
+
+    return function (lang) {
+      return modes[lang] ? CodeMirror.getMode({}, modes[lang]) : 'clike';
+    };
+  }());
+
   MathJax.Hub.processUpdateTime = 200;
   MathJax.Hub.processUpdateDelay = 15;
   MathJax.Hub.Config({
     skipStartupTypeset: true,
+    messageStyle: "none",
     jax: ["input/TeX", "output/HTML-CSS"],
     tex: { extensions: ['color.js', 'extpfeil.js'] },
     tex2jax: {
@@ -20,7 +63,11 @@ jQuery(function ($) { $(document).ready(function(){
     sanitize: true,
     smartLists: true,
     smartypants: true,
-    langPrefix: 'lang-'
+    langPrefix: 'lang-',
+    highlight: function(code, lang) {
+      CodeMirror.runMode(code, getCMMode(lang), gDiv[0]);
+      return '<span class="cm-s-default">' + gDiv.html() + '</span>';
+    }
   });
 
   $('textarea').each(function (index, area) {
@@ -29,7 +76,7 @@ jQuery(function ($) { $(document).ready(function(){
       mode: 'gfm',
       theme: 'default',
       tabSize: 2,
-      autoFocus: true,
+      autofocus: true,
       lineNumbers: false,
       lineWrapping: true,
       matchBrackets: true,
