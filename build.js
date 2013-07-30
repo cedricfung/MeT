1ac0d4f5abf53400c5137437e436a8e27e0fcec0
Cedric Fung <cedric@vec.io>
2013-07-31 02:23:54 +0800

    Use r.js and build.js to compile project

---
+++ build.js
@@ -0,0 +1,15 @@
+({
+  appDir: ".",
+  baseUrl: "javascripts",
+  dir: "../repo.io-dist",
+  paths: {
+    apps: 'apps',
+    jquery: 'libs/jquery-2.0.3',
+    cm: 'libs/codemirror-3.14',
+    jax: 'libs/MathJax-2.2',
+    marked: 'libs/marked',
+  },
+  modules: [{
+    name: "app",
+  }]
+})
--- javascripts/apps/worker.js
+++ javascripts/apps/worker.js
@@ -1,25 +1,31 @@
-importScripts('../libs/marked.js');
+importScripts('../require.js');
 
-marked.setOptions({
-  gfm: true,
-  tables: true,
-  breaks: false,
-  pedantic: false,
-  sanitize: true,
-  smartLists: true,
-  smartypants: true,
-  langPrefix: 'lang-',
+require.config({
+  baseUrl: '../libs',
 });
 
-addEventListener('message', function(e) {
-  var data = e.data;
-  switch (data.cmd) {
-    case 'check': {
-      var result = marked(data.text);
-      postMessage({text: result});
-      break;
+require(['marked'], function(marked){
+  marked.setOptions({
+    gfm: true,
+    tables: true,
+    breaks: false,
+    pedantic: false,
+    sanitize: true,
+    smartLists: true,
+    smartypants: true,
+    langPrefix: 'lang-',
+  });
+
+  addEventListener('message', function(e) {
+    var data = e.data;
+    switch (data.cmd) {
+      case 'check': {
+        var result = marked(data.text);
+        postMessage({text: result});
+        break;
+      }
+      default:
+        break;
     }
-    default:
-      break;
-  }
-}, false);
+  }, false);
+});
