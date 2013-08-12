b28dd3fbd085d6b58b18e846f9b18fbce02b9fb5
Cedric Fung <cedric@vec.io>
2013-08-12 19:25:12 +0800

    Use zepto got get gist, without own ugly jsonp

--- javascripts/apps/github.js
+++ javascripts/apps/github.js
@@ -13,23 +13,21 @@
     });
   };
 
-  // http://stackoverflow.com/questions/9154026/jquery-dynamically-load-a-gist-embed
   return function(ele) {
     var id = $(ele).data('id');
     if (gistCaching[id]) {
       renderGist(ele, gistCaching[id]);
     } else {
-      window["gist_callback_count"] = (window["gist_callback_count"] || 0) + 1;
-      var callbackName = "gist_callback_" + window["gist_callback_count"];
-      window[callbackName] = function (gist) {
-        renderGist(ele, gist);
-        gistCaching[id] = gist;
-        script.parentNode.removeChild(script);
-        delete window[callbackName];
-      };
-      var script = document.createElement("script");
-      script.setAttribute("src", "https://gist.github.com/" + id + ".json?callback=" + callbackName);
-      document.body.appendChild(script);
+      $.ajax({
+        url: "https://gist.github.com/" + id + ".json",
+        dataType: "jsonp",
+        async: false,
+        withCredentials: true,
+        success: function(gist) {
+          gistCaching[id] = gist;
+          renderGist(ele, gist);
+        }
+      });
     }
   };
 
